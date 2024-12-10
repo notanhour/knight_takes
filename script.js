964c29a704834c6044500b946493afb0dc6a70df
APeshko <m2415803@edu.misis.ru>
2024-12-10 21:03:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// Здесь можно добавить JavaScript функционал (например, обработка событий)
-console.log("Добро пожаловать на сайт шахматных задач!");
+//console.log("Добро пожаловать на сайт шахматных задач!");
+document.getElementById("button1").addEventListener("click", function() {
+    window.location.href = "podsite/ofline.html";
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    window.location.href = "podsite/online.html";
+});
