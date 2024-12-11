0840147e5dd2417b8e74e511b1b0061967cbb354
APeshko <m2415803@edu.misis.ru>
2024-12-11 23:31:52 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-//console.log("Добро пожаловать на сайт шахматных задач!");
-document.getElementById("button1").addEventListener("click", function() {
-    window.location.href = "podsite/ofline.html";
-});
-
-document.getElementById("button2").addEventListener("click", function() {
-    window.location.href = "podsite/online.html";
-});
